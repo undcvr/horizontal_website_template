4992ba6ab2326a4a71c7d8b81c4e24427be865b1
undcvr0 <diyor.iknu@gmail.com>
2025-08-26 15:27:04 +0500

    deleted useless wheel data checker

--- static/debug.js
+++
@@ -1,5 +0,0 @@
-document.body.addEventListener('wheel', function() {
-    document.querySelector('.debug-value').innerText = scroll_pos
-    // document.querySelector('.debug-value').innerText = lines_x.length
-})
-
